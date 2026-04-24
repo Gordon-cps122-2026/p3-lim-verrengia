15de2c413d6c6be67a0330b92065a58cd5c4d55c
Jimin Lim <jimin@Jimins-MBP-1326.gordon.edu>
2026-04-24 16:46:58 -0400

    implemented all methods in CheckedOut, testing

--- Library/app/src/main/java/library/model/CheckedOut.java
+++ Library/app/src/main/java/library/model/CheckedOut.java
@@ -4,8 +4,49 @@ import java.util.*;
 
 public class CheckedOut {
 
-    private Book book;
-    private Borrower borrower;
-    private Calendar dueDate;
+  private Book book;
+  private Borrower borrower;
+  private Calendar dueDate;
+  private boolean renewed;
 
+  public CheckedOut(Book book, Borrower borrower) {
+    this.book = book;
+    this.borrower = borrower;
+    this.renewed = false;
+
+    Calendar now = Calendar.getInstance();
+    now.add(Calendar.DAY_OF_MONTH, 28);
+    this.dueDate = now;
+  }
+
+  public Book getBook() {
+    return book;
+  }
+
+  public Borrower getBorrower() {
+    return borrower;
+  }
+
+  public String getDueDate() {
+    return dueDate.get(Calendar.MONTH)
+        + " "
+        + dueDate.get(Calendar.DAY_OF_MONTH)
+        + " "
+        + dueDate.get(Calendar.YEAR);
+  }
+
+  public boolean isRenewed() {
+    return renewed;
+  }
+
+  public boolean renewDueDate() {
+    if (renewed == true) {
+        return false;
+    }
+
+    dueDate.add(dueDate.get(Calendar.DAY_OF_MONTH), 28);
+    renewed = true;
+
+    return true;
+  }
 }
---
+++ Library/app/src/test/java/library/model/CheckedOutTest.java
@@ -0,0 +1,8 @@
+package library.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CheckedOutTest {
+
+}
